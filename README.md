b83daea8d6aa700af698b114af75f9bc77755a8f
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-04-07 10:24:48 -0300

    cadastro

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 npx expo install firebase
 npm install @react-native-async-storage/async-storage
 npm install @react-native-google-signin/google-signin
+npm install @react-native-google-signin/google-signin
--- src/screens/Cadastro.js
+++ src/screens/Cadastro.js
@@ -1,13 +1,90 @@
-import { Button, StyleSheet, TextInput, View } from "react-native";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { useState } from "react";
+import { Alert, Button, StyleSheet, TextInput, View } from "react-native";
+import { auth } from "../../firebase.config";
 
-export default function Cadastro() {
+export default function Cadastro({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [nome, setNome] = useState("");
+
+  const cadastrar = async () => {
+    if (!email || !senha || !nome) {
+      Alert.alert("Atenção", "Preecha todos os campos");
+      return;
+    }
+
+    try {
+      const contaUsuario = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        senha
+      );
+
+      if (contaUsuario.user) {
+        await updateProfile(auth.currentUser, { displayName: nome });
+        // teste user {displayName : "undefined"}
+        console.log(contaUsuario.user);
+      }
+
+      Alert.alert("Cadastro", " Seu cadastro foi concluido com sucesso!", [
+        {
+          style: "cancel",
+          text: "Ficar aqui mesmo",
+          onPress: () => {
+            return;
+          },
+        },
+        {
+          style: "default",
+          text: "Ir para a área logada ",
+          onPress: () => navigation.replace("AreaLogada"),
+        },
+      ]);
+    } catch (error) {
+      console.error(error.code);
+      let mensagem;
+      switch (error.code) {
+        case "auth/email-already-in-use":
+          mensagem = "Email já existente!";
+          break;
+        case "auth/weak-password":
+          mensagem = "Senha fraca!";
+          break;
+        case "auth/invalid-email":
+          mensagem = "Endereço de e-mail inválido!";
+          break;
+        default:
+          mensagem = "Houve um erro, tente mais tarde!";
+          break;
+      }
+      Alert.alert("Ops!", mensagem);
+    }
+  };
+
   return (
     <View style={estilos.container}>
       <View style={estilos.formulario}>
-        <TextInput placeholder="E-mail" style={estilos.input} />
-        <TextInput placeholder="Senha" style={estilos.input} />
+        <TextInput
+          placeholder="Nome"
+          style={estilos.input}
+          keyboardType="default"
+          onChangeText={(valor) => setNome(valor)}
+        />
+        <TextInput
+          placeholder="E-mail"
+          style={estilos.input}
+          keyboardType="email-address"
+          onChangeText={(valor) => setEmail(valor)}
+        />
+        <TextInput
+          placeholder="Senha"
+          style={estilos.input}
+          secureTextEntry
+          onChangeText={(valor) => setSenha(valor)}
+        />
         <View style={estilos.botoes}>
-          <Button title="Cadastre-se" color="blue" />
+          <Button onPress={cadastrar} title="Cadastre-se" color="blue" />
         </View>
       </View>
     </View>
