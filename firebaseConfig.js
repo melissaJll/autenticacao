c6b9636228b0566f32bc137c82effdbd1c1b9840
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-04-06 23:34:14 -0300

    adicionando sdk web

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -9,16 +9,13 @@ import { getAuth } from "firebase/auth"; // Importe o serviço de autenticação
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+  apiKey: "AIzaSyCG-cLXqXjKZya_HY7fgsi6-jixLSMB1t4",
+  authDomain: "cycletrack-ts.firebaseapp.com",
+  projectId: "cycletrack-ts",
+  storageBucket: "cycletrack-ts.appspot.com",
+  messagingSenderId: "619328327967",
+  appId: "1:619328327967:web:0d0856de25bbe25003202a",
 };
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
