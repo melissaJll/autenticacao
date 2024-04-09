82efb311ceee61c8071ddc152188c1dbd8e9a227
Melissa Julia L. L <melissa.lequipe@gmail.com>
2024-04-09 01:41:07 -0300

    Update Inicial.js

--- src/screens/Inicial.js
+++ src/screens/Inicial.js
@@ -4,7 +4,6 @@ export default function Inicial({ navigation }) {
   return (
     <View style={estilos.container}>
       <View style={estilos.introducao}>
-        <Text style={estilos.titulo}>React Native</Text>
         <Text style={estilos.subtitulo}>Autenticação com Firebase</Text>
       </View>
       <View style={estilos.botoes}>
